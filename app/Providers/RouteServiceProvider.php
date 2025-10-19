806521d70ec5c409eb9dc251c00ffeeb371d749e
moinul <moinulibr@gmail.com>
2025-10-19 22:21:41 +0600

    removing extra note on route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,12 +7,9 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class RouteServiceProvider extends ServiceProvider
 {
-    // ... (অন্যান্য কোড, যেমন: HOME প্রপার্টি) ...
 
     public function boot(): void
     {
-        // Alias রেজিস্ট্রেশনের লজিকটি এখান থেকে সরিয়ে দিন
-        // শুধু ডিফল্ট রুট কনফিগারেশন রাখুন:
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
