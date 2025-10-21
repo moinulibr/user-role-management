ee39d2cd4256b1ba0074585ae83c37ece957acd6
moinul <moinulibr@gmail.com>
2025-10-21 22:31:44 +0600

    sidebar blade modify

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,21 +1,20 @@
 @props(['menuItems'])
 {{-- 
     Blade Component (x-sidebar)
-    এই কম্পোনেন্টটি admin-layout.blade.php থেকে $menuItems ডেটা গ্রহণ করে সাইডবার তৈরি করে।
-    এটি resources/views/components/sidebar.blade.php ফাইলে সংরক্ষণ করতে হবে।
+    This component renders the sidebar content - admin-layout.blade.php
 --}}
 <ul class="nav sidebar-inner" id="sidebar-menu">
     
-    {{-- মেনু আইটেমগুলির উপর লুপ --}}
+    {{-- Looping on menuItems --}}
     @foreach ($menuItems as $item)
 
-        {{-- চেক করুন এটি সাবমেনু নাকি সিঙ্গেল মেনু --}}
+        {{-- check if item has submenu --}}
         @if (isset($item['submenu']))
             
-            {{-- ১. সাবমেনু আইটেম (has-sub) --}}
+            {{-- 1. submenu item (has-sub) --}}
             
             @php
-                // সক্রিয় ক্লাস নির্ণয়ের জন্য লজিক: যদি সাবমেনুর কোনো একটি রুটও বর্তমান রুট হয়, তবে গ্রুপটিকে active করুন 
+                // active class logic: if any of submenu route is current route, then group will be active
                 $isActive = false;
                 foreach ($item['submenu'] as $sub) {
                     if (isset($sub['route']) && request()->routeIs($sub['route'] . '*')) {
@@ -28,7 +27,7 @@
             
             <li class="has-sub {{ $isActive ? 'active' : '' }}">
                 
-                {{-- প্রধান ড্রপডাউন লিংক --}}
+                {{-- main dropdown link --}}
                 <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#menu-{{ $menuId }}" 
                    aria-expanded="{{ $isActive ? 'true' : 'false' }}" aria-controls="menu-{{ $menuId }}">
                     <i class="{{ $item['icon'] }}"></i>
@@ -36,11 +35,11 @@
                     <b class="caret"></b>
                 </a>
                 
-                {{-- সাবমেনু কন্টেন্ট --}}
+                {{-- submenu content --}}
                 <ul class="collapse {{ $isActive ? 'show' : '' }} submenu" id="menu-{{ $menuId }}" data-parent="#sidebar-menu">
                     <div class="sub-menu">
                         @foreach ($item['submenu'] as $subItem)
-                            {{-- প্রতিটি সাব-আইটেম: বর্তমান রুট হলে active ক্লাস যোগ করা হলো --}}
+                            {{--every submenu item: add active class if current route --}}
                             <li class="{{ isset($subItem['route']) && request()->routeIs($subItem['route']) ? 'active' : '' }}">
                                 <a class="sidenav-item-link" href="{{ isset($subItem['route']) ? route($subItem['route']) : '#' }}">
                                     <span class="nav-text">{{ __($subItem['title']) }}</span>
@@ -53,7 +52,7 @@
 
         @else
             
-            {{-- ২. সিঙ্গেল মেনু আইটেম --}}
+            {{-- 2. Single menu item --}}
             <li class="{{ isset($item['route']) && request()->routeIs($item['route'] . '*') ? 'active' : '' }}">
                 <a class="sidenav-item-link" href="{{ isset($item['route']) ? route($item['route']) : '#' }}">
                     <i class="{{ $item['icon'] }}"></i>
